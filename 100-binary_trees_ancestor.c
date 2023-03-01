9b548e7d5f94a4a3ba67a3516e064ba41f245be7
Adefemi Adelugba <adefemiade3662@gmail.com>
2023-03-01 11:14:23 +0100

    Binary Trees

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,20 +1,26 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+/**
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
+ *
+ * Return: If no common ancestors return NULL, else return common ancestor.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
 {
-    if (first == NULL || second == NULL)
-        return NULL;
+	binary_tree_t *mom, *pop;
 
-    if (first == second)
-        return (binary_tree_t *)first;
+	if (!first || !second)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-    binary_tree_t *left = binary_trees_ancestor(first, second->left);
-    binary_tree_t *right = binary_trees_ancestor(first, second->right);
-
-    if (left && right)
-        return (binary_tree_t *)second;
-
-    return left ? left : right;
-
-    return (0);
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
 }
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,20 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - finds the sibling of a node
- * @node: pointer to the node to find the sibling
+ * binary_tree_sibling - Finds the sibling of a
+ *                       node in a binary tree.
+ * @node: A pointer to the node to find the sibling of.
  *
- * Return: pointer to the sibling node, or NULL if node has no sibling or if node or its parent are NULL
+ * Return: If node is NULL or there is no sibling - NULL.
+ *         Otherwise - a pointer to the sibling.
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
-    
-    if (node == node->parent->left)
-        return (node->parent->right);
-    else
-        return (node->parent->left);
-
-        return (0);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
 }
